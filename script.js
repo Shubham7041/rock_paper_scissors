c4137c989dd6dc84ee79abec80fb776fcd85a8b9
Shubham <wasnikar.shubham@gmail.com>
2021-09-21 15:33:52 +0530

    Need further work on display of results

--- script.js
+++ script.js
@@ -1,45 +1,56 @@
 let playerSelection;
 let computerSelection;
+let count = 0;
 
 const computerPlay = () => {
   let randomnumber = Number(Math.floor(Math.random() * 3));
   let computerChoice;
   switch (randomnumber) {
     case 0:
-      computerChoice = "rock";
+      computerChoice = "Rock";
       break;
     case 1:
-      computerChoice = "paper";
+      computerChoice = "Paper";
       break;
     case 2:
-      computerChoice = "scissor";
+      computerChoice = "Scissor";
       break;
   }
   return computerChoice;
 };
 
 const playRound = (playerSelection, computerSelection) => {
-  playerSelection = window.prompt("Choose Rock, Paper, Scissor").toLowerCase();
   computerSelection = computerPlay();
-  if (playerSelection == "rock" && computerSelection == "paper") {
+  if (playerSelection == "Rock" && computerSelection == "Paper") {
     return "You Lose! Paper beats Rock";
-  } else if (playerSelection == "paper" && computerSelection == "scissor") {
+  } else if (playerSelection == "Paper" && computerSelection == "Scissor") {
     return "You Lose! Scissor beats Paper";
-  } else if (playerSelection == "scissor" && computerSelection == "rock") {
+  } else if (playerSelection == "Scissor" && computerSelection == "Rock") {
     return "You Lose! Rock beats Scissor";
-  } else if (playerSelection == "scissor" && computerSelection == "paper") {
+  } else if (playerSelection == "Scissor" && computerSelection == "Paper") {
+    count++;
     return "You Win! Scissor beats Paper";
-  } else if (playerSelection == "rock" && computerSelection == "scissor") {
+  } else if (playerSelection == "Rock" && computerSelection == "Scissor") {
+    count++;
     return "You Win! Rock beats Scissor";
-  } else if (playerSelection == "paper" && computerSelection == "rock") {
+  } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    count++;
     return "You Win! Paper beats Rock";
   }
 };
 
-const game = () => {
-  for (let i = 0; i < 5; i++) {
+document.body.addEventListener("click", (event) => {
+  if (event.target.nodeName == "BUTTON") {
+    playerSelection = event.target.value;
     console.log(playRound(playerSelection, computerSelection));
   }
+  if (count == 5) {
+    return "Congrats you won Rounds";
+  }
+});
+/*const game = () => {
+  console.log(playRound(playerSelection, computerSelection));
 };
 
 game();
+*/
